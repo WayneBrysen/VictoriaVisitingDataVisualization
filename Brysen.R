1f848e1f9f50ac8258a7f4969e22c1a6de0ee702
Carson <cyanRaindrop@outlook.com>
2024-10-21 13:03:15 +1100

    adopt template

--- Brysen.R
+++ Brysen.R
@@ -8,6 +8,9 @@ data <- read.csv("dataSet/landmarks-and-places-of-interest-including-schools-the
 set.seed(123) 
 selected_data <- data[sample(1:nrow(data), 10), ]
 
+image_urls <- c("Parking/VictoriaStCarPark6.jpg", "Parking/1_Ynx2iGvAGw2JvM17IXad4g.jpg", "Parking/1575299567-parking.avif","Parking/c870x524.jpg", "Parking/Town-of-Victoria-Park-Stock-Photos-2023-275.jpg", "Parking/OIP.jpg", "Parking/Parking-scaled.jpg")  
+
+
 ui <- navbarPage(
   title = "Discover Melbourne",
   header = setUpTableauInShiny(),
@@ -124,7 +127,55 @@ ui <- navbarPage(
   ),
   
   tabPanel("Placeholder 1"),
-  tabPanel("Placeholder 2"),
+  tabPanel(
+    "Parking Area",
+    fluidPage(
+      fluidRow(
+        tags$div(
+          style = "position: relative; width: 100%; height: 600px; overflow: hidden;",
+          tags$img(
+            src = "drone-aerial-view-of-parking.jpg",
+            style = "width: 100%; height: 100%; object-fit: cover; position: absolute; top: 0; left: 0; z-index: -1;"
+          ),
+          tags$h1(
+            "Victoria. Parking Area", 
+            style = "position: absolute; bottom: 10%; left: 2%; transform: translate(0, 0);
+                     color: white; font-size: 40px; font-weight: bold; padding: 10px; margin: 0;
+                     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);"
+          ),
+          tags$p(
+            "Discover the vibrant city of Melbourne, where art meets coffee culture, and historic landmarks blend with modern skyscrapers. Whether you're exploring hidden laneways or enjoying iconic beaches, Melbourne promises a memorable adventure.",
+            style = "position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);
+                     color: white; font-size: 30px; font-weight: bold; padding: 10px; margin: 0; width: 80%; text-align: center;
+                     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);"
+          )
+        )
+      ),
+      
+      # Carouse
+      fluidRow(
+        h2('Parking Area'),
+        div(class = "carousel",
+            lapply(1:length(image_urls), function(i) {
+              div(
+                class = "carousel-item",
+                tags$img(src = image_urls[i], alt = paste("Image", i)),
+              )
+            })
+        )
+      ),
+      
+      # Tableau
+      fluidRow(
+        h2('Parking Statistics'),
+        tableauPublicViz(
+          id = 'tableauViz',       
+          url = 'https://public.tableau.com/shared/FBBQJXDWC?:display_count=n&:origin=viz_share_link',
+          height = "800px"
+        )
+      )
+    )
+  ),
   tabPanel("Placeholder 3")
 )
 
